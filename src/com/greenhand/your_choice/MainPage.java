144293291a3556efec97d29615e86abb969f56d8
ZHANG JIAZHEN <zasek415@gmail.com>
2014-11-21 21:38:32 +0800

    转盘与第一个页面合并成功，同时有点击转动的按钮
    学长你那个显示bug我还是不知道怎么办。。。

--- src/com/greenhand/your_choice/MainPage.java
+++ src/com/greenhand/your_choice/MainPage.java
@@ -1,5 +1,6 @@
 package com.greenhand.your_choice;
 
+import com.greenhand.your_choice.SlyderView;
 import com.greenhand.your_choice.R;
 import android.os.Bundle;
 import android.app.Activity;
@@ -9,6 +10,7 @@ import android.content.Intent;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -18,6 +20,7 @@ public class MainPage extends Activity {
 
 	private ImageButton buttonToRight;
 	private ImageButton buttonToLeft;
+	private Button		buttonForRoll;	//This will be deleted in the future
 	private ImageButton addButton;
 	private RelativeLayout action;
 	private View first;
@@ -38,6 +41,10 @@ public class MainPage extends Activity {
 		action = (RelativeLayout)findViewById(R.id.action);
 		action.addView(first);
 		
+		LinearLayout part = (LinearLayout)findViewById(R.id.shan);
+		SlyderView slyder = new SlyderView(this);
+		part.addView(slyder);
+		
 		showInf = (TextView)findViewById(R.id.InfTextView);
 		showInf.setText("您输入的选项为："+s);
 		
@@ -46,6 +53,9 @@ public class MainPage extends Activity {
 		
 		buttonToLeft = (ImageButton)findViewById(R.id.buttonToLeft);
 		buttonToLeft.setOnClickListener(new ButtonListenerToLeft());
+		
+		buttonForRoll = (Button)findViewById(R.id.button_spin);
+		buttonForRoll.setOnClickListener(new ButtonListenerForRoll(slyder));
 		
 		addButton = (ImageButton)findViewById(R.id.addButton);
 		addButton.setOnClickListener(new ButtonListenerAdd());				
@@ -92,7 +102,15 @@ public class MainPage extends Activity {
 		}
 		
 	}
-	
+	class ButtonListenerForRoll implements OnClickListener{
+		SlyderView tmp;
+		public ButtonListenerForRoll(SlyderView slyder) {
+			tmp = slyder;
+		}
+		public void onClick(View arg0){
+			tmp.showAnimation(tmp);
+		}
+	}
 	
 	
 	@Override
